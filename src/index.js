da482a648ed6836b834fae381d1b517ae4459f1a
Franssen <ngoiefranssen04@gmail.com>
2023-01-04 12:01:27 +0200

    Header with mu@muimui

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './Components/App/App'
 import store from './redux/store';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const rootElement = ReactDOM.createRoot(document.getElementById('root'));
 rootElement.render(
