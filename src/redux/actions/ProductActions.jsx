6ce38b7ad196a95a3e9693d4db342e83c67496a3
Franssen <ngoiefranssen04@gmail.com>
2023-01-04 10:09:52 +0200

    Wip

---
+++ src/redux/actions/ProductActions.jsx
@@ -0,0 +1,18 @@
+import { ActionTypes } from '../contants/ActionTypes'
+import React from 'react'
+
+const setProducts = (products) => {
+  return (
+    type : ActionTypes.SET_PRODUCTS,
+    payload : products
+  )
+}
+
+const selectedPrduts = (product) => {
+    return (
+      type: ActionTypes.SELECTED_PRODUCTS,
+      payload : product,
+    )
+  }
+
+export default ProductActions
---
+++ src/redux/contants/ActionTypes.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const ActionTypes = {
+  // definitions of the methods to be used
+  SET_PRODUCTS : "SET_PRODUCTS",
+  SELECTED_PRODUCTS : 'SELECTED_PRODUCTS',
+  REMOVE_SELECTED_PRODUCTS : 'REMOVE_SELECTED_PRODUCTS'
+}
