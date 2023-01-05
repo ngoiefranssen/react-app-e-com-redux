beb539627dda7dd669449e1efa2ec0867cc4c1e8
Franssen <ngoiefranssen04@gmail.com>
2023-01-05 11:57:58 +0200

    Wip

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './Components/App/App'
 import store from './redux/store';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const rootElement = ReactDOM.createRoot(document.getElementById('root'));
 rootElement.render(
